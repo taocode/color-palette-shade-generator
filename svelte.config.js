e39310273db94fd9d1abbcad99b248c3981beda8
taocode <mark@taocode.com>
2022-06-25 09:23:26 -0400

    feat: firebase hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import autoImport from 'sveltekit-autoimport'
 import WindiCSS from 'vite-plugin-windicss'
 
