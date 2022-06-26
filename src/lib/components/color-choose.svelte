ebad4605daf05a396daf7a102aa82c773d750d33
taocode <mark@taocode.com>
2022-06-26 13:14:07 -0400

    feat: simplified shades()

--- src/lib/components/color-choose.svelte
+++ src/lib/components/color-choose.svelte
@@ -5,33 +5,33 @@ export let h = 0
 export let s = 75
 export let l = 50
 export let a = 1
-export let steps = 5
+export let steps = 10
 export let stepFactor = 0.05
 
-const colorArray = (color, steps, stepFactor, invert = 1) => {
-	let arr = []
-	for (let i = 1; i <= steps; i++) {
-		arr.push(darken(color,i*stepFactor*(invert)))
+const shades = (color, steps, stepFactor) => {
+	let arr = [color]
+	for (let i = 1; i < steps; i++) {
+		if (i%2) {
+			arr.unshift(darken(color,(i+1)/2*stepFactor*-1))
+		} else {
+			arr.push(darken(color,i/2*stepFactor))
+		}
 	}
 	return arr
 }
 
 let color = "red"
 let contrast = "white"
-let complimentary = "green"
-let lighter = []
-let darker = []
-let compLight = []
-let compDark = []
+let complementary = "green"
+let primaryShades = []
+let complementaryShades = []
 let scheme = "Complementary"
 $: {
 	color = `hsla(${h},${s}%,${l}%,${a})`
 	contrast = readableColor(color)
-	complimentary = `hsla(${h+128},${s}%,${l}%,${a})`
-	lighter = colorArray(color,steps,stepFactor,-1).reverse()
-	darker = colorArray(color,steps,stepFactor)
-	compLight = colorArray(complimentary,steps,stepFactor,-1).reverse()
-	compDark = colorArray(complimentary,steps,stepFactor)
+	complementary = `hsla(${h+128},${s}%,${l}%,${a})`
+	primaryShades = shades(color,steps,stepFactor)
+	complementaryShades = shades(complementary,steps,stepFactor)
 }
 </script>
 
@@ -78,18 +78,12 @@ $: {
 </div>
 
 <div class="shades">
-	{#each lighter as bc}
-	<div class="swatch" style="background-color: {bc}; color: {readableColor(bc)}">{bc.split(',')[2]}</div>
-	{/each}
-	{#each darker as bc}
+	{#each primaryShades as bc}
 	<div class="swatch" style="background-color: {bc}; color: {readableColor(bc)}">{bc.split(',')[2]}</div>
 	{/each}
 </div>
 <div class="flex flex-wrap text-center">
-	{#each compLight as bc}
-	<div class="swatch" style="background-color: {bc}; color: {readableColor(bc)}">{bc.split(',')[2]}</div>
-	{/each}
-	{#each compDark as bc}
+	{#each complementaryShades as bc}
 	<div class="swatch" style="background-color: {bc}; color: {readableColor(bc)}">{bc.split(',')[2]}</div>
 	{/each}
 </div>
@@ -99,7 +93,7 @@ $: {
 		@apply block flex items-center;
 	}
 	.swatch {
-		@apply p-2 min-h-16 min-w-16 flex flex-grow items-center justify-center;
+		@apply p-2 min-h-16 min-w-1/11 flex flex-grow items-center justify-center;
 	}
 	input {
 		@apply text-right  text-2xl border-transparent;
