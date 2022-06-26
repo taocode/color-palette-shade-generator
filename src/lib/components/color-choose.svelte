6c294ba470b3f4f4b42af57ec7d9cfaa474c398c
taocode <mark@taocode.com>
2022-06-26 09:39:57 -0400

    feat: complementary color scheme

--- src/lib/components/color-choose.svelte
+++ src/lib/components/color-choose.svelte
@@ -2,8 +2,8 @@
 import {darken, readableColor} from 'color2k'
 
 export let h = 0
-export let s = '66.6%'
-export let l = '50%'
+export let s = 75
+export let l = 50
 export let a = 1
 export let steps = 5
 export let stepFactor = 0.05
@@ -16,61 +16,98 @@ const colorArray = (color, steps, stepFactor, invert = 1) => {
 	return arr
 }
 
-let darker = []
+let color = "red"
+let contrast = "white"
+let complimentary = "green"
 let lighter = []
-
-$: color = `hsla(${h},${s},${l},${a})`
-$: contrast = 'white'
+let darker = []
+let compLight = []
+let compDark = []
+let scheme = "Complementary"
 $: {
-darker = colorArray(color,steps,stepFactor)
-lighter = colorArray(color,steps,stepFactor,-1)
+	color = `hsla(${h},${s}%,${l}%,${a})`
+	contrast = readableColor(color)
+	complimentary = `hsla(${h+128},${s}%,${l}%,${a})`
+	lighter = colorArray(color,steps,stepFactor,-1).reverse()
+	darker = colorArray(color,steps,stepFactor)
+	compLight = colorArray(complimentary,steps,stepFactor,-1).reverse()
+	compDark = colorArray(complimentary,steps,stepFactor)
 }
 </script>
 
 <div class="p-10 flex" style="color: {contrast}; background-color: {color};">
-	<div class="">
-		<label>Color
-			<input class=" text-center w-full font-bold text-2xl border-transparent" style="background-color: {color};" bind:value={color} placeholder="hsl() rgb() #HEX">
-		</label>
+	<div class="text-2xl font-bold">
+		hsla(
+			<input class="hsla" style="background-color: {color};" 
+			bind:value={h} placeholder="Hue"
+			type="number">,
+			<input class="hsla" style="background-color: {color};" bind:value={s} placeholder="Saturation"
+			type="number">%,
+			<input class="hsla" style="background-color: {color};" bind:value={l} placeholder="luminosity"
+			type="number">%,
+			<input class="hsla" style="background-color: {color};" bind:value={a} placeholder="Hue"
+			type="number">
+		)
+		
 	</div>
 	<div class="">
 		<label>
-			Steps
-			<input class="text-red-700" bind:value={steps}
+			<input style="background-color: {color};" bind:value={steps}
 			type="number"
 			placeholder="steps">
+			Steps
 		</label>
 		<label>
-			Step Factor (0..1)
-			<input class="text-red-700" 
+			<input style="background-color: {color};"
 			bind:value={stepFactor}
 			type="number"
 			min={0}
 			max={1}
 			step={0.01}
 			placeholder="">
+			Step Factor (0..1)
+		</label>
+		<label>
+			<input style="background-color: {color};"
+			class="max-w-[12ch]"
+			bind:value={scheme}
+			placeholder="Monochromatic, Analagous, Complementary, Split-Complementary, Triad, Tetradic">
+			Color Scheme
 		</label>
 	</div>
 </div>
 
-<div class="flex flex-wrap text-center">
+<div class="shades">
 	{#each lighter as bc}
 	<div class="swatch" style="background-color: {bc}; color: {readableColor(bc)}">{bc.split(',')[2]}</div>
 	{/each}
-</div>
-<div class="flex flex-wrap text-center">
 	{#each darker as bc}
 	<div class="swatch" style="background-color: {bc}; color: {readableColor(bc)}">{bc.split(',')[2]}</div>
 	{/each}
 </div>
-
+<div class="flex flex-wrap text-center">
+	{#each compLight as bc}
+	<div class="swatch" style="background-color: {bc}; color: {readableColor(bc)}">{bc.split(',')[2]}</div>
+	{/each}
+	{#each compDark as bc}
+	<div class="swatch" style="background-color: {bc}; color: {readableColor(bc)}">{bc.split(',')[2]}</div>
+	{/each}
+</div>
 
 <style lang="postcss">
 	label {
-		@apply block;
-
+		@apply block flex items-center;
 	}
 	.swatch {
 		@apply p-2 min-h-16 min-w-16 flex flex-grow items-center justify-center;
+	}
+	input {
+		@apply text-right  text-2xl border-transparent;
+	}
+	.hsla {
+		@apply font-bold max-w-[4ch];
+	}
+	.shades {
+		@apply flex flex-wrap text-center;
 	}
 </style>
