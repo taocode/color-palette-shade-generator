9c53c9bc287b1b9d05fbffcf066bebf4d17e6c00
taocode <mark@taocode.com>
2022-07-13 16:47:55 -0400

    feat: improved fancy title

--- src/lib/components/color-palette-shade-generator.svelte
+++ src/lib/components/color-palette-shade-generator.svelte
@@ -1,14 +1,28 @@
 <script>
 export const name = 'Color Palette Shade Generator'
+export const size = '1.25em'
+export let grid = true
 export const fanciful = name
   .split(' ')
     .reduce(
         (p,c) => {
           const fl = c.substring(0,1)
           const r = c.substring(1)
-          return `${p} <span class="inline-block py-1 px-3 aspect-square text-center align-baseline font-800 ${fl}">${fl}</span>${r}`
+          return `${p} <div class="max-w-min whitespace-nowrap mr-2"><span class="CPSG ${fl}" style="height: ${size}; width: ${size};">${fl}</span>${r}</div>`
         },''
       )
 </script>
 
-{@html fanciful}
+<div class="max-w-fit mx-auto">
+  <div class="max-w-60 mx-auto flex-wrap justify-center sm:(inline-flex flex-nowrap max-w-none grid-cols-none)"
+  class:grid={grid} class:grid-cols-2={grid} class:inline-flex={!grid} 
+  >
+    {@html fanciful}
+  </div>
+</div>
+
+<style lang="postcss">
+  .inflex {
+    @apply inline-flex;
+  }
+</style>
