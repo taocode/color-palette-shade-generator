97aacac1e58c563f70e5f67d4b39d6f1e1369adc
Mark Jones <mark@taocode.com>
2022-07-18 23:19:02 -0400

    feat: improved color on var inputs

--- src/lib/components/color-patch.svelte
+++ src/lib/components/color-patch.svelte
@@ -14,38 +14,39 @@ export let description = 'Color'
 export let shades = ['white']
 export let schemeIndex = 0
 
-let hidden = true
+let hidden = false
 let includeDefault = false
 
 $: lastShade = shades[shades.length-1]
 $: inputColor = lighten(color,0.30)
-$: placeholderColor = lighten(color,0.2)
 $: hue = parseToHsla(color)[0].toFixed()
 $: $colorNames[schemeIndex] = name
 </script>
 
 <div class="name" style="
 background: {lastShade};
-color: {readableColor(lastShade)}; 
+color: {shades[0]}; 
 background: linear-gradient(90deg, {lastShade} 10%, {shades[0]} 90%;">
   <input bind:value={name}
     placeholder={hueName(hue)}
     size={5}
     class="varName"
-    style="background:{lastShade}; --color-placeholder:{lighten(color,0.25)};">
+    style="--color-background:{lastShade}; 
+    --color-foreground: {shades[0]}; 
+    --color-placeholder:{shades[1]};">
 {#if description}<em>({description} = {hue}°)</em>{/if}
   <button class="border border-transparent hover:border-current rounded p-1 text-xs"
   on:click={() => { hidden = ! hidden }}>{@html dots}</button>
   <div class="fixed hidden bg-dark-900 bg-opacity-80 inset-0 flex z-10" class:hidden>
     <div class="details"
       use:clickOutside={() => hidden = true}
-      style="--color-background: {color}; --color-foreground: {readableColor(color)}">
+      style="--color-background: {color}; --color-foreground: {shades[0]}">
       <div class="var-title">
-        <label class="py-1 pl-2" style="color: {color}; background-color: {readableColor(color)};">
+        <label class="" style="color: {color}; background-color: {lastShade};">
           var:
-          <input placeholder={hueName(hue)} bind:value={name} class="border-1 px-1"
+          <input placeholder={hueName(hue)} bind:value={name} class="border-1 ml-1"
           size={name.length}
-          style="--color-placeholder: {darken(color,0.3)}; background-color: {inputColor}; color: {readableColor(inputColor)}; border-color: {readableColor(inputColor)};">
+          style="--color-placeholder: {darken(color,0.175)}; --color-background: {inputColor}; --color-foreground: {darken(color,0.25)}; border-color: {readableColor(inputColor)};">
         </label>
         <button title="close" class="close flex" on:click={() => hidden = true}><XIcon size="1.5x" /></button>
       </div>
@@ -75,6 +76,18 @@ background: linear-gradient(90deg, {lastShade} 10%, {shades[0]} 90%;">
   .close {
     @apply bg-white text-dark-300 rounded-sm float-right;
   }
+  label {
+    @apply text-xl p-1 pl-2 inline-block;
+  }
+  input {
+    background-color: var(--color-background);
+    color: var(--color-foreground);
+    @apply font-500 max-w-30 border-0 pl-2;
+  }
+  ::placeholder {
+    color: var(--color-placeholder);
+    font-weight: 300;
+  }
   .varName {
     /* @apply w-max-w-16; */
   }
@@ -87,9 +100,6 @@ background: linear-gradient(90deg, {lastShade} 10%, {shades[0]} 90%;">
     max-h-[90vh] max-w-[95vw] overflow-auto
     ;
     background-color: var(--color-background);
-  }
-  ::placeholder {
-    color: var(--color-placeholder);
   }
   .var-panels {
     @apply lg:(grid gap-2 grid-cols-3)
