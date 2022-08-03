ebc0bd69190daf10d5241bb8c81ea088fb9adadc
taocode <mark@taocode.com>
2022-08-03 03:32:22 -0400

    feat: improved dark/light gradient

--- src/lib/components/gradient-display.svelte
+++ src/lib/components/gradient-display.svelte
@@ -3,14 +3,17 @@ import { adjustHue, darken, lighten } from 'color2k'
 import { schemes, schemeColors } from '$lib'
 import { primaryColor, scheme } from '$lib/stores'
 
-$: sColors = schemeColors(schemes[$scheme],$primaryColor) 
-$: c0 = $scheme <= 1 ? darken($primaryColor,0.3) : $primaryColor
-$: c1 = $scheme <= 1 ? lighten($primaryColor,0.3) : sColors[1].color
-$: c2 = $scheme > 2 ? sColors[2].color : 'transparent'
-$: c3 = $scheme > 5 ? sColors[3].color : 'transparent'
+$: _si = $scheme
+$: _pc = $primaryColor
+$: _sCs = schemeColors(schemes[_si],_pc) 
+$: c0 = _si < 1 ? darken(_pc,0.3) : _si === 1 ? darken(_pc,0.36) :  _pc
+$: c1 = _si < 1 ? lighten(_pc,0.3) : _si === 1 ? darken(_pc,0.025) : _sCs[1].color
+$: c2 = _si === 1 ? lighten(_pc,0.025) : _si > 2 ? _sCs[2].color : 'transparent'
+$: c3 = _si === 1 ? lighten(_pc,0.33): _si > 5 ? _sCs[3].color : 'transparent'
+
 </script>
 
-<div class="gradient-display scheme-{$scheme}"
+<div class="gradient-display scheme-{_si}"
 style="
 --c0: {c0};
 --c1: {c1};
