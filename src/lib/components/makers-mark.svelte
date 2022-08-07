6f4d2f5dbc675070dd0969c919d4daad63f5529f
taocode <mark@taocode.com>
2022-08-07 18:04:30 -0400

    fix: improved word wrap

--- src/lib/components/makers-mark.svelte
+++ src/lib/components/makers-mark.svelte
@@ -4,12 +4,13 @@
 <div class="placer"
 style="--color-primary: {$primaryColor}">
 	<div class="maker">
-		Created by <a target="_blank" href="https://www.taocode.com/">TAOCODE</a>; powered by curiosity and <a target="_blank" href="https://svelte.dev/">Svelte</a>.
+		<span>Created by <a target="_blank" href="https://www.taocode.com/">TAOCODE</a></span>
+		<span>Powered by curiosity and <a target="_blank" href="https://svelte.dev/">Svelte</a></span>
 	</div>
 </div>
 <style lang="postcss">
 	.maker {
-		@apply max-w-prose mx-auto text-center bg-white/60 p-4 italic opacity-60 transition duration-200 ease-out hover:opacity-100;
+		@apply max-w-prose mx-auto text-center bg-white/60 p-4 italic opacity-75 transition duration-200 ease-out hover:opacity-100;
 		&:focus-within {
 			@apply opacity-100;
 		}
@@ -20,6 +21,9 @@ style="--color-primary: {$primaryColor}">
 				color: var(--color-primary);
 			}
 		}
+	}
+	span {
+		@apply whitespace-nowrap;
 	}
 	.placer {
 		@apply absolute bottom-0 left-0 right-0;
