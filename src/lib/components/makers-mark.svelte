ce550828da709053921c6f0d1f6e681f5bdde858
taocode <mark@taocode.com>
2022-08-21 02:14:14 -0400

    feat: extra darkening for lower hues

--- src/lib/components/makers-mark.svelte
+++ src/lib/components/makers-mark.svelte
@@ -1,16 +1,22 @@
 <script>
-	import { primaryColor } from '$lib/stores'
+	import { darken } from 'color2k'
+	import { hue, primaryColor } from '$lib/stores'
+
+	$: lessDim = ($hue/360.0)*0.05
+	$: console.log({lessDim})
+	$: colorPrimary = darken($primaryColor,0.2-lessDim)
 </script>
 <div class="placer"
-style="--color-primary: {$primaryColor}">
+style="--color-primary: {colorPrimary}">
 	<div class="maker">
 		<span>Created by <a target="_blank" class="text-lime-700" href="https://www.taocode.com/">TAOCODE</a></span>.
-		<span>Powered by curiosity and <a target="_blank" class="text-orange-700" href="https://svelte.dev/">Svelte</a></span>.
+		<span>Driven by curiosity. Powered by <a target="_blank" class="text-orange-700" href="https://svelte.dev/">Svelte</a></span>.
 	</div>
 </div>
 <style lang="postcss">
 	.maker {
-		@apply max-w-prose mx-auto text-center bg-white/60 p-4 italic opacity-75 transition duration-200 ease-out hover:opacity-100;
+		@apply max-w-prose mx-auto text-center bg-white/60 p-4 italic opacity-75 transition duration-200 ease-out hover:opacity-100
+		rounded-tr rounded-tl;
 		&:focus-within {
 			@apply opacity-100;
 		}
