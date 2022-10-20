b776970ffa1b698db53990ec37d833f2688a5a39
taocode <mark@taocode.com>
2022-10-20 16:52:01 -0400

    feat: accessible sliders using input range

--- src/lib/components/hsla-slider.svelte
+++ src/lib/components/hsla-slider.svelte
@@ -1,23 +1,23 @@
 <script>
-	import { adjustHue } from 'color2k'
-	import Slider from '@bulatdashiev/svelte-slider'
-	import { hue, saturation } from '$lib/stores'
-	
-	export let prop = 'S'
-	export let store = saturation
+	const propNames = {
+		'H':'Hue',
+		'S':'Saturation',
+		'L':'Lightness',
+		'A':'Alpha'
+	}
+	export let prop = 'H'
+	export let store
 	export let max = 1
 	export let step = 0.001
 	// let thumbBg = ``
-	let _vals = [$store,max]
-	$: _vals = [$store,max]
-	$: store.set(_vals[0])
 	let style = `--prop: '${prop}';`
 
+	let propName = propNames[prop]
 	import './slider.pcss'
 
 </script>
 
 <div class="text-center">
-	<input class="hsla-slider {prop}" {style} type="range" id="slider-{prop}" bind:value={$store} min={0} {max} {step}/>
+	<input aria-label="{propName} Slider" class="hsla-slider {prop}" {style} type="range" id="slider-{prop}" bind:value={$store} min={0} {max} {step}/>
 </div>
 
